906eae1516fcbdd21a2f88e2a3d4a53e8b6e31e9
Dmitry Shtanko <lempionnewliving@gmail.com>
2021-10-31 16:51:28 +0300

    refactor(handlers): Decomposed command handlers

--- telegram/handlers.go
+++ telegram/handlers.go
@@ -6,19 +6,18 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-const commandStart = "start"
+const (
+	commandStart = "start"
+
+	replyStart = "Hi! To save links to your Pocket account, first you need to give me access to it. To do this, follow the link:\n%s"
+)
 
 func (b *Bot) handleCommand(message *tgbotapi.Message) error {
-	msg := tgbotapi.NewMessage(message.Chat.ID, "You entered unknown command")
-
 	switch message.Command() {
 	case commandStart:
-		msg.Text = "You entered /start command"
-		_, err := b.bot.Send(msg)
-		return err
+		return b.handleStartCommand(message)
 	default:
-		_, err := b.bot.Send(msg)
-		return err
+		return b.handleUnknownCommand(message)
 	}
 
 }
@@ -28,4 +27,18 @@ func (b *Bot) handleMessage(message *tgbotapi.Message) {
 
 	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
 	b.bot.Send(msg)
+}
+
+func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "You entered /start command")
+
+	_, err := b.bot.Send(msg)
+	return err
+}
+
+func (b *Bot) handleUnknownCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "You entered unknown command")
+
+	_, err := b.bot.Send(msg)
+	return err
 }
