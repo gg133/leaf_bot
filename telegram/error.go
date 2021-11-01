8a1a3754bc1807a881527f9ef0bdcf6d029cd5c4
Dmitry Shtanko <lempionnewliving@gmail.com>
2021-11-01 22:13:46 +0300

    doc(error): Added documentaion for error functions

--- telegram/error.go
+++ telegram/error.go
@@ -6,14 +6,16 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+//Variables of custom error messages
 var (
 	errInvaildURL   = errors.New("url is invalid")
 	errUnauthorized = errors.New("user is not authorized")
 	errUnableToSave = errors.New("unable to save")
 )
 
+//handleError send in telegram an error message depending on the error itself
 func (b *Bot) handleError(chatID int64, err error) {
-	msg := tgbotapi.NewMessage(chatID, "An unknown error has occurred")
+	msg := tgbotapi.NewMessage(chatID, b.messages.Default)
 
 	switch err {
 	case errInvaildURL:
