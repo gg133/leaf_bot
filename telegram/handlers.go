1cef6eee2931b63d73f4a4b9166a0a6aff065447
Dmitry Shtanko <lempionnewliving@gmail.com>
2021-11-01 19:23:42 +0300

    feat(handler): Added logic for URL saving from bot

--- telegram/handlers.go
+++ telegram/handlers.go
@@ -1,9 +1,11 @@
 package telegram
 
 import (
-	"log"
+	"context"
+	"net/url"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/yalagtyarzh/leaf_bot/pocket"
 )
 
 //Constants for command handling
@@ -15,6 +17,7 @@ const (
 const (
 	replyStartTemplate     = "Hi! To save links to your Pocket account, first you need to give me access to it. To do this, follow the link:\n%s"
 	replyAlreadyAuthorized = "You are already authorized! Send me link and I will save it."
+	replyAddSuccess        = "Link saved successfully!"
 )
 
 //handleCommand method handle telegram commands (message, which starts from "/")
@@ -28,12 +31,34 @@ func (b *Bot) handleCommand(message *tgbotapi.Message) error {
 
 }
 
-//handleMessage method handle messages (in this case, it sends back a message written by the user before.)
-func (b *Bot) handleMessage(message *tgbotapi.Message) {
-	log.Printf("[%s] %s", message.From.UserName, message.Text)
+//handleMessage method checks for url validation in tg message, gets access token from db and if everything is good url saves in Pocket
+func (b *Bot) handleMessage(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, replyAddSuccess)
 
-	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
-	b.bot.Send(msg)
+	_, err := url.ParseRequestURI(message.Text)
+	if err != nil {
+		msg.Text = "This is an invalid link!"
+		_, err := b.bot.Send(msg)
+		return err
+	}
+
+	accessToken, err := b.getAccessToken(message.Chat.ID)
+	if err != nil {
+		msg.Text = "You are not logged in! Use the command /start"
+		_, err := b.bot.Send(msg)
+		return err
+	}
+	if err := b.pocketClient.Add(context.Background(), pocket.AddInput{
+		AccessToken: accessToken,
+		URL:         message.Text,
+	}); err != nil {
+		msg.Text = "Oops, the link could not be saved. Please try again later."
+		_, err := b.bot.Send(msg)
+		return err
+	}
+
+	_, err = b.bot.Send(msg)
+	return err
 }
 
 //handleStartCommand method generates authentification link for pocket and sends message with it link
